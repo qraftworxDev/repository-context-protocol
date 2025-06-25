cf670853adada00565da5e350d0db4764417791a
Coetzee van Staden <cstaden@credable.io>
2025-06-25 12:40:33 +0100

    chore: fix unneeded custom helper

--- internal/ast/python/type_mapping_test.go
+++ internal/ast/python/type_mapping_test.go
@@ -1,6 +1,7 @@
 package python
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -275,17 +276,5 @@ def complex_generic_function(
 
 // Helper function to check if a string contains a substring (case-insensitive)
 func contains(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr ||
-		len(s) > len(substr) && (s[:len(substr)] == substr ||
-			s[len(s)-len(substr):] == substr ||
-			findSubstring(s, substr)))
-}
-
-func findSubstring(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
+	return strings.Contains(s, substr)
 }
